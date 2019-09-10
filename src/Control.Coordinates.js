1cbf403ec57b414fe2a9ad2604f95d483eb56b9b
Chaim Krause <chaim.e.krause.civ@mail.mil>
2019-09-10 15:37:43 -0500

    Cleans up using StandardJS

--- src/Control.Coordinates.js
+++ src/Control.Coordinates.js
@@ -1,7 +1,7 @@
-"use strict";
+'use strict'
 
 /**
- * author Michal Zimmermann <zimmicz@gmail.com>
+ * author Chaim Krause <chaim@chaim.com> (prior author Michal Zimmermann <zimmicz@gmail.com>)
  * Displays coordinates of mouseclick.
  * @param object options:
  *        position: bottomleft, bottomright etc. (just as you are used to it with Leaflet)
@@ -10,71 +10,68 @@
  *        promptText: text displayed when user clicks the control
  *        precision: number of decimals to be displayed
  */
+import L from 'leaflet'
+
 L.Control.Coordinates = L.Control.extend({
-	options: {
-		position: 'bottomleft',
-		latitudeText: 'lat.',
-		longitudeText: 'lon.',
-		promptText: 'Press Ctrl+C to copy coordinates',
-		precision: 4
-	},
+  options: {
+    position: 'bottomleft',
+    latitudeText: 'lat.',
+    longitudeText: 'lon.',
+    promptText: 'Press Ctrl+C to copy coordinates',
+    precision: 4
+  },
 
-	initialize: function(options)
-	{
-		L.Control.prototype.initialize.call(this, options);
-	},
+  initialize: function (options) {
+    L.Control.prototype.initialize.call(this, options)
+  },
 
-	onAdd: function(map)
-	{
-		var className = 'leaflet-control-coordinates',
-			that = this,
-			container = this._container = L.DomUtil.create('div', className);
-		this.visible = false;
+  onAdd: function (map) {
+    var className = 'leaflet-control-coordinates'
+    var that = this
+    var container = this._container = L.DomUtil.create('div', className)
+    this.visible = false
 
-			L.DomUtil.addClass(container, 'hidden');
+    L.DomUtil.addClass(container, 'hidden')
 
+    L.DomEvent.disableClickPropagation(container)
 
-		L.DomEvent.disableClickPropagation(container);
+    this._addText(container, map)
 
-		this._addText(container, map);
+    L.DomEvent.addListener(container, 'click', function () {
+      var lat = L.DomUtil.get(that._lat)
+      var lng = L.DomUtil.get(that._lng)
+      var latTextLen = this.options.latitudeText.length + 1
+      var lngTextLen = this.options.longitudeText.length + 1
+      var latTextIndex = lat.textContent.indexOf(this.options.latitudeText) + latTextLen
+      var lngTextIndex = lng.textContent.indexOf(this.options.longitudeText) + lngTextLen
+      var latCoordinate = lat.textContent.substr(latTextIndex)
+      var lngCoordinate = lng.textContent.substr(lngTextIndex)
 
-		L.DomEvent.addListener(container, 'click', function() {
-			var lat = L.DomUtil.get(that._lat),
-				lng = L.DomUtil.get(that._lng),
-				latTextLen = this.options.latitudeText.length + 1,
-				lngTextLen = this.options.longitudeText.length + 1,
-				latTextIndex = lat.textContent.indexOf(this.options.latitudeText) + latTextLen,
-				lngTextIndex = lng.textContent.indexOf(this.options.longitudeText) + lngTextLen,
-				latCoordinate = lat.textContent.substr(latTextIndex),
-				lngCoordinate = lng.textContent.substr(lngTextIndex);
+      window.prompt(this.options.promptText, latCoordinate + ' ' + lngCoordinate)
+    }, this)
 
-			window.prompt(this.options.promptText, latCoordinate + ' ' + lngCoordinate);
-    	}, this);
+    return container
+  },
 
-		return container;
-	},
+  _addText: function (container, context) {
+    this._lat = L.DomUtil.create('span', 'leaflet-control-coordinates-lat', container)
+    this._lng = L.DomUtil.create('span', 'leaflet-control-coordinates-lng', container)
 
-	_addText: function(container, context)
-	{
-		this._lat = L.DomUtil.create('span', 'leaflet-control-coordinates-lat' , container),
-		this._lng = L.DomUtil.create('span', 'leaflet-control-coordinates-lng' , container);
+    return container
+  },
 
-		return container;
-	},
+  /**
+   * This method should be called when user clicks the map.
+   * @param event object
+   */
+  setCoordinates: function (obj) {
+    if (!this.visible) {
+      L.DomUtil.removeClass(this._container, 'hidden')
+    }
 
-	/**
-	 * This method should be called when user clicks the map.
-	 * @param event object
-	 */
-	setCoordinates: function(obj)
-	{
-		if (!this.visible) {
-			L.DomUtil.removeClass(this._container, 'hidden');
-		}
-
-		if (obj.latlng) {
-			L.DomUtil.get(this._lat).innerHTML = '<strong>' + this.options.latitudeText + ':</strong> ' + obj.latlng.lat.toFixed(this.options.precision).toString();
-			L.DomUtil.get(this._lng).innerHTML = '<strong>' + this.options.longitudeText + ':</strong> ' + obj.latlng.lng.toFixed(this.options.precision).toString();
-		}
-	}
-});
+    if (obj.latlng) {
+      L.DomUtil.get(this._lat).innerHTML = '<strong>' + this.options.latitudeText + ':</strong> ' + obj.latlng.lat.toFixed(this.options.precision).toString()
+      L.DomUtil.get(this._lng).innerHTML = '<strong>' + this.options.longitudeText + ':</strong> ' + obj.latlng.lng.toFixed(this.options.precision).toString()
+    }
+  }
+})
